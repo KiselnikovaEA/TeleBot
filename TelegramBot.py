4ea3dd45548b8c571e8ed55bddac4337d5a3611e
KisEA <maslennikvakat@mail.ru>
2022-12-18 09:56:50 +0300

    Удаление старых комментариев

--- TelegramBot.py
+++ TelegramBot.py
@@ -11,12 +11,6 @@ config = dotenv_values(".env")
 bot = Bot(config['TOKEN'])
 dp = Dispatcher(bot)
 
-# Клавиатура с обычными кнопками
-b1 = KeyboardButton('/Хочу')
-b2 = KeyboardButton('/Обойдусь')
-kb_answer = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-kb_answer.add(b1).add(b2)
-
 horo_sign = {
     'Овен': 'https://horo.mail.ru/prediction/aries/today/',
     'Телец': 'https://horo.mail.ru/prediction/taurus/today/',
@@ -32,26 +26,17 @@ horo_sign = {
     'Рыбы': 'https://horo.mail.ru/prediction/pisces/today/'
     }
 
+# Клавиатура с обычными кнопками
+b1 = KeyboardButton('/Хочу')
+b2 = KeyboardButton('/Обойдусь')
+kb_answer = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+kb_answer.add(b1).add(b2)
+
+# Инлайн-кнопки
 kb_horoscope = InlineKeyboardMarkup(row_width=1)
 for sign in horo_sign:
     urlButton = InlineKeyboardButton(text = sign, url=horo_sign[sign])
     kb_horoscope.add(urlButton)
-# Инлайн-кнопки
-# kb_horoscope = InlineKeyboardMarkup(row_width=1)
-# urlButton1 = InlineKeyboardButton(text='Овен', url='https://horo.mail.ru/prediction/aries/today/')
-# urlButton2 = InlineKeyboardButton(text='Телец', url='https://horo.mail.ru/prediction/taurus/today/')
-# urlButton3 = InlineKeyboardButton(text='Близнецы', url='https://horo.mail.ru/prediction/gemini/today/')
-# urlButton4 = InlineKeyboardButton(text='Рак', url='https://horo.mail.ru/prediction/cancer/today/')
-# urlButton5 = InlineKeyboardButton(text='Лев', url='https://horo.mail.ru/prediction/leo/today/')
-# urlButton6 = InlineKeyboardButton(text='Дева', url='https://horo.mail.ru/prediction/virgo/today/')
-# urlButton7 = InlineKeyboardButton(text='Весы', url='https://horo.mail.ru/prediction/libra/today/')
-# urlButton8 = InlineKeyboardButton(text='Скорпион', url='https://horo.mail.ru/prediction/scorpio/today/')
-# urlButton9 = InlineKeyboardButton(text='Стрелец', url='https://horo.mail.ru/prediction/sagittarius/today/')
-# urlButton10 = InlineKeyboardButton(text='Козерог', url='https://horo.mail.ru/prediction/capricorn/today/')
-# urlButton11 = InlineKeyboardButton(text='Водолей', url='https://horo.mail.ru/prediction/aquarius/today/')
-# urlButton12 = InlineKeyboardButton(text='Рыбы', url='https://horo.mail.ru/prediction/pisces/today/')
-# kb_horoscope.add(urlButton1, urlButton2, urlButton3, urlButton4, urlButton5, urlButton6, urlButton7, urlButton8, urlButton9, urlButton10, urlButton11, urlButton12)
-
 
 @dp.message_handler(commands=['start'])
 async def command_start(message : types.Message):
